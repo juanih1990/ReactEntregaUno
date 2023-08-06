dfff9452baa34d6decacb08deb5f27daf6b78eab
juan Hernandez <juanihernandez1990@gmail.com>
2023-08-06 18:53:07 -0300

    pequeños ajustes

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -3,13 +3,14 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
+                {/* <a class="navbar-brand" href="#">Navbar</a> */}
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className={`nav-link active ${styles['custom-input']}`}  aria-current="page" href="#">TECNOLOGIA</a>
+                            <a className={`nav-link active ${styles['custom-input']}`} aria-current="page" href="#">TECNOLOGIA</a>
                         </li>
                         <li className="nav-item">
                             <a className={`nav-link active ${styles['custom-input']}`} aria-current="page" href="#">HOGAR</a>
@@ -20,7 +21,6 @@ const NavBar = () => {
                         <li className="nav-item">
                             <a className={`nav-link active ${styles['custom-input']}`} aria-current="page" href="#">CLIMATIZACION</a>
                         </li>
-                        
                     </ul>
                 </div>
             </div>
